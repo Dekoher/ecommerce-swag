c6df50d378fee3bf6ac856e7d4e417c3d54b1066
Diegoher <diegoherr92@gmail.com>
2020-08-23 17:15:57 -0500

    finidhing modules 5-> Modules, Routes & Guards, modularizing components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,38 +5,28 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product/product.component';
-import { ExponentialPipe } from './pipes/exponential.pipe';
-import { HighlightDirective } from './directives/highlight.directive';
-import { HomeComponent } from './pages/home/home.component';
 import { ProductsComponent } from './pages/products/products.component';
 import { ContactComponent } from './pages/contact/contact.component';
-import { HeaderComponent } from './components/header/header.component';
-import { BannerComponent } from './components/banner/banner.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { NotFoundComponent } from './components/not-found/not-found.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { LayoutComponent } from './pages/layout/layout.component';
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
-    ExponentialPipe,
-    HighlightDirective,
-    HomeComponent,
     ProductsComponent,
     ContactComponent,
-    HeaderComponent,
-    BannerComponent,
-    FooterComponent,
-    NotFoundComponent,
     ProductDetailComponent,
     LayoutComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    SharedModule,
+    CoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
