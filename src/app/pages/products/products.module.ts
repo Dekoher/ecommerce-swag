479d89fdfd2d223d423ff0b9a21f9d59a4bde5ba
Diegoher <diegoherr92@gmail.com>
2020-08-24 13:23:40 -0500

    modularizing product-detail and strating with angular-material

--- src/app/pages/products/products.module.ts
+++ src/app/pages/products/products.module.ts
@@ -3,17 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { ProductComponent } from './components/product/product.component';
 import { ProductsComponent } from './components/products/products.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 
 import { ProductsRoutingModule } from './products-routing.module';
+import { MaterialModule } from '../../material/material.module';
 
 @NgModule({
   declarations: [
     ProductComponent,
     ProductsComponent,
+    ProductDetailComponent
   ],
   imports: [
     CommonModule,
     ProductsRoutingModule,
+    MaterialModule,
   ],
 })
 
