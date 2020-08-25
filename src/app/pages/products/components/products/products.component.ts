8fe28e45d5e67c65ddce91e425af0cae0cbb7e9a
Diegoher <diegoherr92@gmail.com>
2020-08-25 12:45:53 -0500

    implementig httpModule in app & getting data from REST API

--- src/app/pages/products/components/products/products.component.ts
+++ src/app/pages/products/components/products/products.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from './product.model';
+import { ProductService } from '../../../../core/services/product/product.service';
 
 @Component({
   selector: 'app-products',
@@ -7,56 +8,63 @@ import { Product } from './product.model';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
-  products: Product[] = [
-    {
-      id: '1',
-      image: 'assets/img/127-0-0-1.jpg',
-      title: 'Localhost',
-      price: 200,
-      description: 'Localhost',
-    },
-    {
-      id: '2',
-      image: 'assets/img/bigCenterAngular.jpg',
-      title: 'Angular',
-      price: 200,
-      description: 'Angular',
-    },
-    {
-      id: '3',
-      image: 'assets/img/bigCenterJs.png',
-      title: 'JavaScript',
-      price: 250,
-      description: 'JavaScript',
-    },
-    {
-      id: '4',
-      image: 'assets/img/MenReactJS.jpg',
-      title: 'React',
-      price: 200,
-      description: 'React',
-    },
-    {
-      id: '5',
-      image: 'assets/img/vue.jpg',
-      title: 'Vue',
-      price: 200,
-      description: 'Vue',
-    },
-    {
-      id: '6',
-      image: 'assets/img/Javascript-Stickers-Mugs.jpg',
-      title: 'Many Stickers',
-      price: 300,
-      description: 'Many Stickers',
-    },
-  ];
-  constructor() { }
+  products: Product[] = [];
+  // products: Product[] = [
+  //   {
+  //     id: '1',
+  //     image: 'assets/img/127-0-0-1.jpg',
+  //     title: 'Localhost',
+  //     price: 200,
+  //     description: 'Localhost',
+  //   },
+  //   {
+  //     id: '2',
+  //     image: 'assets/img/bigCenterAngular.jpg',
+  //     title: 'Angular',
+  //     price: 200,
+  //     description: 'Angular',
+  //   },
+  //   {
+  //     id: '3',
+  //     image: 'assets/img/bigCenterJs.png',
+  //     title: 'JavaScript',
+  //     price: 250,
+  //     description: 'JavaScript',
+  //   },
+  //   {
+  //     id: '4',
+  //     image: 'assets/img/MenReactJS.jpg',
+  //     title: 'React',
+  //     price: 200,
+  //     description: 'React',
+  //   },
+  //   {
+  //     id: '5',
+  //     image: 'assets/img/vue.jpg',
+  //     title: 'Vue',
+  //     price: 200,
+  //     description: 'Vue',
+  //   },
+  //   {
+  //     id: '6',
+  //     image: 'assets/img/Javascript-Stickers-Mugs.jpg',
+  //     title: 'Many Stickers',
+  //     price: 300,
+  //     description: 'Many Stickers',
+  //   },
+  // ];
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
+    this.fetchProducts();
   }
 
-  addTocart(id): void {
-    console.log('id=>', id);
+  addTocart(id: number): void {
+  }
+
+  fetchProducts(): void {
+    this.productService.getAllProducts().subscribe(products => {
+      this.products = products;
+    });
   }
 }
