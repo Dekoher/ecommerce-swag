ab75076f5af944bfb40a3c1362d3f98698f2e8f7
Diegoher <diegoherr92@gmail.com>
2020-08-27 21:51:38 -0500

    finishing module 7 - services

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,23 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { ProductFormComponent } from './components/product-form/product-form.component';
-import { ReactiveFormsModule } from '@angular/forms';
-
 import { MaterialModule } from '../material/material.module';
 import { NavComponent } from './components/nav/nav.component';
-import { LayoutModule } from '@angular/cdk/layout';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { InventoryComponent } from './components/inventory/inventory.component';
+import { ProductsListComponent } from './components/products-list/products-list.component';
 
 @NgModule({
-  declarations: [ProductFormComponent, NavComponent, DashboardComponent],
+  declarations: [ProductFormComponent, NavComponent, DashboardComponent, InventoryComponent, ProductsListComponent],
   imports: [
     CommonModule,
     AdminRoutingModule,
     ReactiveFormsModule,
     MaterialModule,
-    LayoutModule,
   ]
 })
 export class AdminModule { }
--- src/app/pages/products/components/product-detail/product-detail.component.ts
+++ src/app/pages/products/components/product-detail/product-detail.component.ts
@@ -27,4 +27,35 @@ export class ProductDetailComponent implements OnInit {
       this.product = product;
     });
   }
+
+  createProduct(): void {
+    const newProduct: Product = {
+      id: '22',
+      title: 'New From Angular Ecommerce',
+      image: 'assets/img/127.0.0.1.jpg',
+      price: 200,
+      description: 'New product',
+    };
+    this.productService.createProduct(newProduct).subscribe( product => {
+      // this.product = product;
+      console.log(product);
+    });
+  }
+
+  updateProduct(): void {
+    const newProduct: Partial<Product> = {
+      title: 'Updated by me',
+      price: 50000,
+      description: 'New product',
+    };
+    this.productService.updateProduct('1', newProduct).subscribe( product => {
+      console.log(product);
+    });
+  }
+
+  deleteProduct(): void {
+    this.productService.deleteProduct('YYYYYY').subscribe( product => {
+      console.log('product deleted=>', product);
+    });
+  }
 }
