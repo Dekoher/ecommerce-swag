520a5e8158f27a82c537a15b7e5a245a354fd7d8
Diegoher <diegoherr92@gmail.com>
2020-08-28 10:20:33 -0500

    refactoring product-model route & adding products to cart functionality with reactive programming

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { CartService } from '../../../core/services/cart/cart.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  // total = 99;
+  total$: Observable<number>;
+  constructor(private cartService: CartService) {
+    // // first way (common subscription) to make reactive the counter badge
+    // this.cartService.cart$.subscribe(products => {
+    //   console.log('products in cart=>', products);
+    //   this.total = products.length;
+    // });
+    // // second way (pipe) to make reactive the counter badge
+    // this.cartService.cart$
+    //   .pipe(map((product) => product.length))
+    //   .subscribe((totalTransformed) => {
+    //     console.log('products in cart=>', totalTransformed);
+    //     this.total = totalTransformed;
+    //   });
+    // // third way (making the subscriptions through the html template with async)
+    // // to make reactive the counter badge
+    this.total$ = this.cartService.cart$.pipe(
+      map(products => products.length)
+    );
   }
 
+  ngOnInit(): void {}
 }
