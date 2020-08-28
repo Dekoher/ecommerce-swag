a3f2d6732b730808c28b5b6b7881bd7380b832d2
Diegoher <diegoherr92@gmail.com>
2020-08-28 02:18:06 -0500

    creating product form

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProductFormComponent } from './components/product-form/product-form.component';
+import { CreateProductFormComponent } from './components/create-product-form/create-product-form.component';
 import { NavComponent } from './components/nav/nav.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { InventoryComponent } from './components/inventory/inventory.component';
 import { ProductsListComponent } from './components/products-list/products-list.component';
+import { EditProductFormComponent } from './components/edit-product-form/edit-product-form.component';
 
 const routes: Routes = [
   {
@@ -26,6 +28,14 @@ const routes: Routes = [
       {
         path: 'products',
         component: ProductsListComponent,
+      },
+      {
+        path: 'products/create',
+        component: CreateProductFormComponent,
+      },
+      {
+        path: 'products/edit/:id',
+        component: EditProductFormComponent,
       },
     ]
   }
