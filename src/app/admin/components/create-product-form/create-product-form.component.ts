8641ae467a0842f96d50b41fdd5aaf2c5a597cda
Diegoher <diegoherr92@gmail.com>
2020-09-03 20:17:23 -0500

    uploading an image to firebase storage

--- src/app/admin/components/create-product-form/create-product-form.component.ts
+++ src/app/admin/components/create-product-form/create-product-form.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AngularFireStorage } from '@angular/fire/storage';
 import { ProductService } from '../../../core/services/product/product.service';
 import { MyValidators } from '../../../utils/validators';
+import { finalize } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-create-product-form',
@@ -11,10 +14,12 @@ import { MyValidators } from '../../../utils/validators';
 })
 export class CreateProductFormComponent implements OnInit {
   form: FormGroup;
+  image$: Observable<any>;
   constructor(
     private formBuilder: FormBuilder,
     private productService: ProductService,
     private router: Router,
+    private storage: AngularFireStorage,
   ) {
     this.buildForm();
   }
@@ -29,6 +34,24 @@ export class CreateProductFormComponent implements OnInit {
       image: [''],
       description: ['', [Validators.required]],
     });
+  }
+
+  uploadFile(event) {
+    const file = event.target.files[0];
+    console.log('event.target=>', event.target);
+    const name = `${file.name}`;
+    const fileRef = this.storage.ref(name);
+    const task = this.storage.upload(name, file);
+    task.snapshotChanges().pipe(
+      finalize(() => {
+        this.image$ = fileRef.getDownloadURL();
+        this.image$.subscribe(url => {
+          console.log('url=>', url);
+          this.form.get('image').setValue(url);
+        });
+      })
+    )
+    .subscribe();
   }
 
   saveProduct(event: Event): void {
